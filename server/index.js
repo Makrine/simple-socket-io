23c596c08c4fa506fc658d66e59ff936f0707c14
Makrina Tskipurishvili <66353680+Makrine@users.noreply.github.com>
2023-11-11 17:23:14 +0400

    separate css and script, add username on conenct

--- server/index.js
+++ server/index.js
@@ -7,12 +7,18 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, '../public')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../app', 'index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    
+    socket.on('initConnection', (data) => {
+      console.log('a user connected: ' + data.username);
+    });
     socket.on('disconnect', () => {
         console.log('user disconnected');
       });
